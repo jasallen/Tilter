1a56ed20079d0519ee519d83ad4f34279d56170b
Jason DeBoever <github@deboever.us>
2017-09-17 22:01:20 -0400

    More Config

--- Tilter/Tilter.XF/AzureIoTHubConfig.cs
+++ Tilter/Tilter.XF/AzureIoTHubConfig.cs
@@ -6,10 +6,16 @@ namespace Tilter.XF
 {
     static class AzureIoTHubConfig
     {
-        public const string DeviceId = "";
+        public const string DeviceId = "JasonsRazPi3";
 
         public const string DeviceKey = "";
 
         public const string IotHubUri = "";
+    }
+    static class AzureSignalRConfig
+    {
+        public const string EndPoint = "";
+
+        public const string HubName = "AccelHub";
     }
 }
