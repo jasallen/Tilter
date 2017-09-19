7106c097d3716b90b9b259e378543c2d627d6059
Jason DeBoever <github@deboever.us>
2017-09-18 21:07:38 -0400

    Add unit test Proj

---
+++ AccelerationAndGyroTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AccelerationAndGyroTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AccelerationAndGyroTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9cf19a1c-17b7-47f5-9bd4-aaebcc2f2425")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AccelerationAndGyroTests/RotationAnglesTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccelerationAndGyro;
+
+namespace AccelerationAndGyroTests
+{
+    [TestClass]
+    public class RotationAnglesTests
+    {
+        [TestMethod]
+        public void UpdateGravity_Should_ResetEulerAngles()
+        {
+            var angles = new RotationAngles(-1, -1, -1);
+
+            var sensors = new AccelerationAndGyroModel()
+            {
+                AccelerationX = 0.75f,
+                AccelerationY = 0f,
+                AccelerationZ = 0.75f,
+                GyroX = 0f,
+                GyroY = 0f,
+                GyroZ = 0f
+            };
+
+            angles.UpdateFromGravity(sensors);
+
+            Assert.IsTrue(angles.Pitch == 359);
+            Assert.IsTrue(angles.Roll == 45);
+            Assert.IsTrue(angles.Yaw == 359);
+        }
+    }
+}
